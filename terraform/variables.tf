5abaf3285fa246a19e0347dc18419b1fb2e8c3cc
mariano lopez <mariano.lopez@uala.com.ar>
2020-10-26 11:34:29 -0300

    gitignore

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region to create things in."
-  default     = "us-east-1"
-}
-
-variable "aws_profile" {
-  description = "The AWS profile account"
-  default = "161142984839_AdministratorAccess"
-}
